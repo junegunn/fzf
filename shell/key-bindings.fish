23244bb41087f4f833c33219bba7a7213710ab20
Junegunn Choi <junegunn.c@gmail.com>
2016-01-14 01:09:16 +0900

    [fish] Fix intermittent errors on CTRL-T
    This seems like a bug of fish, but sometimes when you select an item
    fish complains:
    
    "insertion mode switches can not be used when not in insertion mode"
    
    This only happens when using tmux pane. Injecting a dummy command
    somehow fixes the issue.

--- shell/key-bindings.fish
+++ shell/key-bindings.fish
@@ -20,6 +20,7 @@ function fzf_key_bindings
       -o -type d -print \
       -o -type l -print 2> /dev/null | sed 1d | cut -b3-"
     eval "$FZF_CTRL_T_COMMAND | "(__fzfcmd)" -m > $TMPDIR/fzf.result"
+    and sleep 0
     and commandline -i (cat $TMPDIR/fzf.result | __fzf_escape)
     commandline -f repaint
     rm -f $TMPDIR/fzf.result
