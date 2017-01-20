174f95690d5a25223a8209caa02d85ffcd4b50ab
DanielFGray <DanielFGray@gmail.com>
2017-01-19 19:22:14 -0600

    key-bindings respect ~/.ignore

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -4,7 +4,11 @@ if [[ $- == *i* ]]; then
 
 # CTRL-T - Paste the selected file path(s) into the command line
 __fsel() {
-  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+  local cmd d ignore_dirs=()
+  while IFS= read -r d; do
+    ignore_dirs+=( -path "\*/$d" -o )
+  done < "$HOME/.ignore"
+  cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( ${ignore_dirs[*]} -path '*/\\.*' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | cut -b3-"}"
