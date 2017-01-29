d85a69a7092ac88bf0609643d9a2ad77d4d69ca9
Junegunn Choi <junegunn.c@gmail.com>
2017-01-30 01:53:17 +0900

    0.16.3

--- src/constants.go
+++ src/constants.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// Current version
-	version = "0.16.2"
+	version = "0.16.3"
 
 	// Core
 	coordinatorDelayMax  time.Duration = 100 * time.Millisecond
--- src/core.go
+++ src/core.go
@@ -3,7 +3,7 @@ Package fzf implements fzf, a command-line fuzzy finder.
 
 The MIT License (MIT)
 
-Copyright (c) 2016 Junegunn Choi
+Copyright (c) 2017 Junegunn Choi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
