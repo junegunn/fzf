3d74d277aa0446264240308e52df6f223cc2a8ed
Junegunn Choi <junegunn.c@gmail.com>
2017-02-17 13:07:45 +0900

    Use cut instead of sed in the default command

--- src/constants_unix.go
+++ src/constants_unix.go
@@ -4,5 +4,5 @@ package fzf
 
 const (
 	// Reader
-	defaultCommand = `find -L . -path '*/\.*' -prune -o -type f -print -o -type l -print 2> /dev/null | sed s/^..//`
+	defaultCommand = `find -L . -path '*/\.*' -prune -o -type f -print -o -type l -print 2> /dev/null | cut -b3-`
 )
