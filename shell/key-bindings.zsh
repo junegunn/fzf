52e68b1d05ffcd8d7dad0bd7f29b9e4bc3e8d408
Yad Smood <y.s.inside@gmail.com>
2018-11-27 18:47:38 +0900

    Suppress the zsh autocomplete line number output

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -67,14 +67,11 @@ bindkey '\ec' fzf-cd-widget
 fzf-history-widget() {
   local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
-  selected=( $(fc -rl 1 |
+  selected=( $(fc -rln 1 |
     FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
   local ret=$?
   if [ -n "$selected" ]; then
-    num=$selected[1]
-    if [ -n "$num" ]; then
-      zle vi-fetch-history -n $num
-    fi
+    LBUFFER="$selected"
   fi
   zle reset-prompt
   return $ret
