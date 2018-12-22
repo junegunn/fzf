55fc0fa1d43a56bbff411d5039350f42f12fcb8e
Aaron Bieber <aaron@bolddaemon.com>
2018-12-22 09:07:51 -0700

    Add pledge(2) support (OpenBSD only) via a 'protector' package.

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/junegunn/fzf/src"
+import (
+	"github.com/junegunn/fzf/src"
+	"github.com/junegunn/fzf/src/protector"
+)
 
 var revision string
 
 func main() {
+	protector.Protect()
 	fzf.Run(fzf.ParseOptions(), revision)
 }
