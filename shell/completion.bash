b80418d06912f43e5a328b23758eff305abead90
RockAndSka <yoann_mac_donald@hotmail.com>
2019-01-10 12:52:51 +0100

    remove unecessary 'echo' into _fzf_compgen_path

--- shell/completion.bash
+++ shell/completion.bash
@@ -12,7 +12,6 @@
 # To use custom commands instead of find, override _fzf_compgen_{path,dir}
 if ! declare -f _fzf_compgen_path > /dev/null; then
   _fzf_compgen_path() {
-    echo "$1"
     command find -L "$1" \
       -name .git -prune -o -name .svn -prune -o \( -type d -o -type f -o -type l \) \
       -a -not -path "$1" -print 2> /dev/null | sed 's@^\./@@'
