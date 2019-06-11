b17330fa296ea4df06f16130d44c5c6e058b1ea1
HongBeenKim <dtb05045@gmail.com>
2019-06-11 12:36:26 +0900

    Add required files for GUI color picker

--- src/tui/tui.go
+++ src/tui/tui.go
@@ -339,6 +339,80 @@ func EmptyTheme() *ColorTheme {
 		Border:       colUndefined}
 }
 
+/*
+ * Add New schemes
+ *
+ * by Kyungyeon
+ *
+
+[quiet light]
+export FZF_DEFAULT_OPTS='
+  --color fg:145,bg:231,hl:97,fg+:59,bg+:188,hl+:97
+  --color info:64,prompt:64,spinner:64,pointer:131,marker:131
+'
+
+
+[monokai dimmend]
+export FZF_DEFAULT_OPTS='
+  --color fg:107,bg:16,hl:166,fg+:188,bg+:59,hl+:166
+  --color info:97,prompt:97,spinner:97,pointer:67,marker:67
+'
+
+
+
+[light]
+export FZF_DEFAULT_OPTS='
+  --color fg:28,bg:231,hl:21,fg+:0,bg+:153,hl+:21
+  --color info:124,prompt:124,spinner:124,pointer:66,marker:66
+'
+
+
+[light+]
+export FZF_DEFAULT_OPTS='
+  --color fg:94,bg:231,hl:128,fg+:16,bg+:153,hl+:128
+  --color info:57,prompt:57,spinner:57,pointer:30,marker:30
+'
+
+
+[kimbie dark]
+export FZF_DEFAULT_OPTS='
+  --color fg:137,bg:16,hl:109,fg+:180,bg+:58,hl+:109
+  --color info:107,prompt:107,spinner:107,pointer:145,marker:145
+'
+
+
+[high contrast]
+export FZF_DEFAULT_OPTS='
+  --color fg:74,bg:16,hl:208,fg+:16,bg+:231,hl+:175
+  --color info:187,prompt:187,spinner:187,pointer:79,marker:79
+'
+
+
+
+[dark]
+export FZF_DEFAULT_OPTS='
+  --color fg:65,bg:235,hl:74,fg+:188,bg+:59,hl+:74
+  --color info:174,prompt:174,spinner:174,pointer:102,marker:102
+'
+
+
+
+[dark+]
+export FZF_DEFAULT_OPTS='
+  --color fg:65,bg:16,hl:187,fg+:188,bg+:59,hl+:174
+  --color info:74,prompt:74,spinner:74,pointer:174,marker:174
+'
+
+
+
+
+[abyss]
+export FZF_DEFAULT_OPTS='
+  --color fg:60,bg:16,hl:212,fg+:35,bg+:17,hl+:212
+  --color info:97,prompt:97,spinner:97,pointer:180,marker:180
+
+*/
+
 func errorExit(message string) {
 	fmt.Fprintln(os.Stderr, message)
 	os.Exit(2)
