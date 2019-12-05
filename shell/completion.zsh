5341e56a47067b740f1b58c861a40969d164f50d
Henré Botha <henrebotha@gmail.com>
2019-12-05 15:23:32 +0100

    ssh completion: handle multiple hosts per line
    This commit allows the ssh completion to handle the case where multiple
    hostnames are declared on one line, such as in this example that
    declares three hosts:
    
    	Host foo.com bar.dev baz
    
    It does not correcly handle quoted hostnames that contain whitespace,
    but this was the case before this commit.

--- shell/completion.zsh
+++ shell/completion.zsh
@@ -115,10 +115,10 @@ _fzf_complete_telnet() {
 _fzf_complete_ssh() {
   _fzf_complete '+m' "$@" < <(
     setopt localoptions nonomatch
-    command cat <(cat ~/.ssh/config ~/.ssh/config.d/* /etc/ssh/ssh_config 2> /dev/null | command grep -i '^host ' | command grep -v '[*?]' | awk '{for (i = 2; i <= NF; i++) print $1 " " $i}') \
-        <(command grep -oE '^[[a-z0-9.,:-]+' ~/.ssh/known_hosts | tr ',' '\n' | tr -d '[' | awk '{ print $1 " " $1 }') \
-        <(command grep -v '^\s*\(#\|$\)' /etc/hosts | command grep -Fv '0.0.0.0') |
-        awk '{if (length($2) > 0) {print $2}}' | sort -u
+    command cat <(cat ~/.ssh/config ~/.ssh/config.d/* /etc/ssh/ssh_config 2> /dev/null | command grep -i '^host ' | command sed 's/^host //gi' | command grep -Eo '('\''|"|\b)\S+\1\b' | command grep -v '[*?]') \
+        <(command grep -oE '^[[a-z0-9.,:-]+' ~/.ssh/known_hosts | tr ',' '\n' | tr -d '[' | awk '{ print $1 " " $1 }' | awk '{if (length($2) > 0) {print $2} else {print $1}}') \
+        <(command grep -v '^\s*\(#\|$\)' /etc/hosts | command grep -Fv '0.0.0.0' | awk '{if (length($2) > 0) {print $2} else {print $1}}') |
+        sort -u
   )
 }
 
