a1bcdc225e1c9b890214fcea3d19d85226fc552a
Aaron Bieber <aaron@bolddaemon.com>
2020-01-18 22:13:32 -0700

    Add pledge(2) support (OpenBSD only) via a 'protector' package. (#1297)

---
+++ src/protector/protector_openbsd.go
@@ -0,0 +1,10 @@
+// +build openbsd
+
+package protector
+
+import "golang.org/x/sys/unix"
+
+// Protect calls OS specific protections like pledge on OpenBSD
+func Protect() {
+	unix.PledgePromises("stdio rpath tty proc exec")
+}
