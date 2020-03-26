198e5c6991c7dddfad3d22342a6f3c0d6e6b8c81
Jack Bates <jack@nottheoilrig.com>
2020-03-26 08:06:28 -0700

    Enable Style/MethodCallWithArgsParentheses

--- src/update_assets.rb
+++ src/update_assets.rb
@@ -7,7 +7,7 @@ require 'json'
 
 if ARGV.length < 3
   puts "usage: #{$PROGRAM_NAME} <token> <version> <files...>"
-  exit 1
+  exit(1)
 end
 
 token, version, *files = ARGV
@@ -18,27 +18,27 @@ rels = JSON.parse(RestClient.get(base, authorization: "token #{token}"))
 rel = rels.find { |r| r['tag_name'] == version }
 unless rel
   puts "#{version} not found"
-  exit 1
+  exit(1)
 end
 
 # List assets
-assets = Hash[rel['assets'].map { |a| a.values_at 'name', 'id' }]
+assets = Hash[rel['assets'].map { |a| a.values_at('name', 'id') }]
 
-files.select { |f| File.exist? f }.map do |file|
+files.select { |f| File.exist?(f) }.map do |file|
   Thread.new do
-    name = File.basename file
+    name = File.basename(file)
 
     if asset_id = assets[name] # rubocop:todo Lint/AssignmentInCondition
       puts "#{name} found. Deleting asset id #{asset_id}."
-      RestClient.delete "#{base}/assets/#{asset_id}",
-                        authorization: "token #{token}"
+      RestClient.delete("#{base}/assets/#{asset_id}",
+                        authorization: "token #{token}")
     else
       puts "#{name} not found"
     end
 
     puts "Uploading #{name}"
     RestClient.post(
-      "#{base.sub 'api', 'uploads'}/#{rel['id']}/assets?name=#{name}",
+      "#{base.sub('api', 'uploads')}/#{rel['id']}/assets?name=#{name}",
       File.read(file),
       authorization: "token #{token}",
       content_type: 'application/octet-stream'
