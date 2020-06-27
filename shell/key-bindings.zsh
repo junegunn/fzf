9b937ad4b015cdc137586693f4c2696ab13721ef
Gibson Fahnestock <gibfahn@gmail.com>
2020-06-27 22:14:21 +0100

    Allow inserting multiple lines from history
    Allows inserting multiple lines from the history at once.
    
    Preserves the existing behaviour, e.g. moving the cursor to the end of
    the input line.
    
    Newline-separates the lines if multiple are specified. I didn't add a
    trailing newline to keep compatibility with the existing implementation.
    
    Confirmed this works with `\t` and `\n` chars, and assuming `echo`,
    being a zsh builtin, will handle the unescaping adequately.
    
    Fixes: https://github.com/junegunn/fzf/issues/1806
    Refs: https://superuser.com/questions/1564346/insert-multiple-lines-from-zsh-history

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -98,17 +98,36 @@ bindkey '\ec' fzf-cd-widget
 
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
-  local selected num
+  local selected num selected_lines selected_line selected_line_arr
   setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
-  selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+
+  # Read history lines (split on newline) into selected_lines array.
+  selected_lines=(
+    "${(@f)$(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
+    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} -m" $(__fzfcmd))}"
+  )
   local ret=$?
-  if [ -n "$selected" ]; then
-    num=$selected[1]
-    if [ -n "$num" ]; then
-      zle vi-fetch-history -n $num
-    fi
+
+  # Remove empty elements, converting ('') to ().
+  selected_lines=($selected_lines)
+  if [[ "${#selected_lines[@]}" -ne 0 ]]; then
+    local -a history_lines=()
+    for selected_line in "${selected_lines[@]}"; do
+      # Split each history line on spaces, and take the 1st value (history line number).
+      selected_line_arr=($=selected_line)
+      num=$selected_line_arr[1]
+      if [[ -n "$num" ]]; then
+        # Add history at line $num to history_lines array.
+        history_lines+=( "$(fc -ln $num $num)" )
+      fi
+    done
+    # Set input buffer to newline-separated list of history lines.
+    # Use echo to unescape, e.g. \n to newline, \t to tab.
+    BUFFER="$(echo ${(F)history_lines})"
+    # Move cursor to end of buffer.
+    CURSOR=$#BUFFER
   fi
+
   zle reset-prompt
   return $ret
 }
