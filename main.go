b5e0e29ec6a149fa948c0399af34572d450d3dda
Junegunn Choi <junegunn.c@gmail.com>
2020-10-29 01:36:55 +0900

    Assign default number version (without patch version)
    So that you can still build and use fzf even when the precise version
    number is not injected via -ldflags.

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"github.com/junegunn/fzf/src/protector"
 )
 
-var version string
-var revision string
+var version string = "0.24"
+var revision string = "devel"
 
 func main() {
-	if len(version) == 0 {
-		panic("Invalid build: version information missing")
-	}
 	protector.Protect()
 	fzf.Run(fzf.ParseOptions(), version, revision)
 }
