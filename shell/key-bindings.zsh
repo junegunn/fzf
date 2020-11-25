46fcda5c42a39897b231ab029d6ea320252e1f39
ratijas <me@ratijas.tk>
2020-11-25 03:39:06 +0300

    zsh: declare variable as local before assignment

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -45,6 +45,7 @@ __fsel() {
     -o -type d -print \
     -o -type l -print 2> /dev/null | cut -b3-"}"
   setopt localoptions pipefail no_aliases 2> /dev/null
+  local item
   eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -m "$@" | while read item; do
     echo -n "${(q)item} "
   done
