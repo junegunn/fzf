c49bc712eae2089b0538c43d5ca964f3c7964991
Vlastimil Ovčáčík <vovcacik@github.ovcacik.org>
2021-10-08 13:28:37 +0200

    Test recursive reading of files and dirs in working directory
    This tests the default command, i.e. readFiles(), on temporary non-empty
    directory.

--- src/reader_test.go
+++ src/reader_test.go
@@ -1,6 +1,9 @@
 package fzf
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -59,5 +62,93 @@ func TestReadFromCommand(t *testing.T) {
 	}
 	if !eb.Peek(EvtReadFin) {
 		t.Error("EvtReadFin should be set")
+	}
+}
+
+// create temporary file structure, run fzf on it and check the files it saw
+func TestReadFiles(t *testing.T) {
+	pushedStrings := []string{}
+	pusher := func(s []byte) bool {
+		pushedStrings = append(pushedStrings, string(s))
+		return true
+	}
+	reader := NewReader(pusher, util.NewEventBox(), false, true)
+
+	// setup test dir
+	testRootPath, err := ioutil.TempDir("", "fzf-test-walk-")
+	if err != nil {
+		t.Errorf("error: %s", err)
+		return
+	}
+	defer os.RemoveAll(testRootPath)
+
+	// create and change to a fzf's working dir
+	originalWD, err := os.Getwd()
+	if err != nil {
+		t.Errorf("error: %s", err)
+		return
+	}
+	defer os.Chdir(originalWD)
+
+	testWD := filepath.Join(testRootPath, "wd")
+	err = os.Mkdir(testWD, 0777)
+	if err != nil {
+		t.Errorf("error: %s", err)
+		return
+	}
+	err = os.Chdir(testWD)
+	if err != nil {
+		t.Errorf("error: %s", err)
+		return
+	}
+
+	// create test files
+	files := []string{
+		"excludedFile",
+		"excludedDir/foo",
+		"wd/includedFile",
+		"wd/includedDir/foo",
+		"wd/includedDir/bar",
+	}
+	for _, relFilePath := range files {
+		absFilePath := filepath.Join(testRootPath, relFilePath)
+		absDirPath, _ := filepath.Split(absFilePath)
+
+		// create all required dirs
+		err = os.MkdirAll(absDirPath, 0777)
+		if err != nil {
+			t.Errorf("error: %s", err)
+		}
+
+		// touch the file
+		absFile, err := os.Create(absFilePath)
+		if err != nil {
+			t.Errorf("error: %s", err)
+		}
+		absFile.Close()
+	}
+
+	// make fzf read the files
+	ok := reader.readFiles()
+	if !ok {
+		t.Error("error: readFiles() indicated error")
+	}
+
+	// check the read files
+	expected := map[string]interface{}{ // used as a set, ignore values
+		`includedFile`:    nil,
+		`includedDir/foo`: nil,
+		`includedDir/bar`: nil,
+	}
+	for _, s := range pushedStrings {
+		s = filepath.ToSlash(s) // windows: normalize path separators for comparison purposes
+		if _, found := expected[s]; found {
+			delete(expected, s)
+		} else {
+			t.Errorf("unexpected file encountered: %s", s)
+		}
+	}
+	for k, _ := range expected {
+		t.Errorf("didn't encounter expected file: %s", k)
 	}
 }
