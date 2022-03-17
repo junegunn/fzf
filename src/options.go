73331a9da8d5c7ed98dbf005c63cc9df44019419
Derek <dsimon_aas@hotmail.com>
2022-03-17 12:56:04 -0700

    updating source file locations to prevent autofail
    temp fix but changing to github/rayiik/fzf so the test in the make file does not fail because options.go and options.go.org differ in source. *temp fix*

--- src/options.go
+++ src/options.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"unicode"
 
-	"github.com/junegunn/fzf/src/algo"
-	"github.com/junegunn/fzf/src/tui"
+	"github.com/rayiik/fzf/src/algo"
+	"github.com/rayiik/fzf/src/tui"
 
 	"github.com/mattn/go-runewidth"
 	"github.com/mattn/go-shellwords"
