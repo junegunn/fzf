de885fe9d9ff2f468ee9dfcbe00f67166559c100
Jan Warchoł <jan.warchol@gmail.com>
2022-04-06 01:12:10 +0200

    Allow passing args to __fzf_select__ via fzf-file-widget
    This makes it easier to make customizations, for example instead of
    
        bind -x '"\C-o\C-i": FZF_CTRL_T_COMMAND="fasd -Rl" FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --tiebreak=index " fzf-file-widget'
    
    it's enough to just
    
        bind -x '"\C-o\C-i": FZF_CTRL_T_COMMAND="fasd -Rl" fzf-file-widget --tiebreak=index'

--- shell/key-bindings.bash
+++ shell/key-bindings.bash
@@ -32,7 +32,7 @@ __fzfcmd() {
 }
 
 fzf-file-widget() {
-  local selected="$(__fzf_select__)"
+  local selected="$(__fzf_select__ "$@")"
   READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
   READLINE_POINT=$(( READLINE_POINT + ${#selected} ))
 }
