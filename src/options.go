b93c3136aca74684c2c5d8b525a3f74b98313a71
Atha <74566464+Pandademic@users.noreply.github.com>
2022-04-18 09:36:21 -0400

    make usage(--help) neater.
    also:
    * clean up atoi error msg.

--- src/options.go
+++ src/options.go
@@ -16,97 +16,97 @@ import (
 )
 
 const usage = `usage: fzf [options]
+  	OPTIONS:
+	  Search:
+	    -x, --extended        Extended-search mode
+				  (enabled by default; +x or --no-extended to disable)
+	    -e, --exact           Enable Exact-match
+	    --algo=TYPE           Fuzzy matching algorithm: [v1|v2] (default: v2)
+	    -i                    Case-insensitive match (default: smart-case match)
+	    +i                    Case-sensitive match
+	    --literal             Do NOT normalize latin script letters before matching
+	    -n, --nth=N[,..]      Comma-separated list of field index expressions
+				  for limiting search scope. Each can be a non-zero
+				  integer or a range expression ([BEGIN]..[END]).
+	    --with-nth=N[,..]     Transform the presentation of each line using
+				  field index expressions
+	    -d, --delimiter=STR   Field delimiter regex (default: AWK-style)
+	    +s, --no-sort         Do NOT sort the result
+	    --tac                 Reverse the order of the input
+	    --disabled            Do NOT* perform search
+	    --tiebreak=CRI[,..]   Comma-separated list of sort criteria to apply
+				  when the scores are tied [length|begin|end|index]
+				  (default: length)
 
-  Search
-    -x, --extended        Extended-search mode
-                          (enabled by default; +x or --no-extended to disable)
-    -e, --exact           Enable Exact-match
-    --algo=TYPE           Fuzzy matching algorithm: [v1|v2] (default: v2)
-    -i                    Case-insensitive match (default: smart-case match)
-    +i                    Case-sensitive match
-    --literal             Do not normalize latin script letters before matching
-    -n, --nth=N[,..]      Comma-separated list of field index expressions
-                          for limiting search scope. Each can be a non-zero
-                          integer or a range expression ([BEGIN]..[END]).
-    --with-nth=N[,..]     Transform the presentation of each line using
-                          field index expressions
-    -d, --delimiter=STR   Field delimiter regex (default: AWK-style)
-    +s, --no-sort         Do not sort the result
-    --tac                 Reverse the order of the input
-    --disabled            Do not perform search
-    --tiebreak=CRI[,..]   Comma-separated list of sort criteria to apply
-                          when the scores are tied [length|begin|end|index]
-                          (default: length)
+	  Interface:
+	    -m, --multi[=MAX]     Enable multi-select with tab/shift-tab
+	    --no-mouse            Disable mouse
+	    --bind=KEYBINDS       Custom key bindings. Refer to the man page.
+	    --cycle               Enable cyclic scroll
+	    --keep-right          Keep the right end of the line visible on overflow
+	    --scroll-off=LINES    Number of screen lines to keep above or below when
+				  scrolling to the top or to the bottom (default: 0)
+	    --no-hscroll          Disable horizontal scroll
+	    --hscroll-off=COLS    Number of screen columns to keep to the right of the
+				  highlighted substring (default: 10)
+	    --filepath-word       Make word-wise movements respect path separators
+	    --jump-labels=CHARS   Label characters for jump and jump-accept
 
-  Interface
-    -m, --multi[=MAX]     Enable multi-select with tab/shift-tab
-    --no-mouse            Disable mouse
-    --bind=KEYBINDS       Custom key bindings. Refer to the man page.
-    --cycle               Enable cyclic scroll
-    --keep-right          Keep the right end of the line visible on overflow
-    --scroll-off=LINES    Number of screen lines to keep above or below when
-                          scrolling to the top or to the bottom (default: 0)
-    --no-hscroll          Disable horizontal scroll
-    --hscroll-off=COLS    Number of screen columns to keep to the right of the
-                          highlighted substring (default: 10)
-    --filepath-word       Make word-wise movements respect path separators
-    --jump-labels=CHARS   Label characters for jump and jump-accept
+	  Layout:
+	    --height=HEIGHT[%]    Display fzf window below the cursor with the given
+				  height instead of using fullscreen
+	    --min-height=HEIGHT   Minimum height when --height is given in percent
+				  (default: 10)
+	    --layout=LAYOUT       Choose layout: [default|reverse|reverse-list]
+	    --border[=STYLE]      Draw border around the finder
+				  [rounded|sharp|horizontal|vertical|
+				   top|bottom|left|right|none] (default: rounded)
+	    --margin=MARGIN       Screen margin (TRBL | TB,RL | T,RL,B | T,R,B,L)
+	    --padding=PADDING     Padding inside border (TRBL | TB,RL | T,RL,B | T,R,B,L)
+	    --info=STYLE          Finder info style [default|inline|hidden]
+	    --prompt=STR          Input prompt (default: '> ')
+	    --pointer=STR         Pointer to the current line (default: '>')
+	    --marker=STR          Multi-select marker (default: '>')
+	    --header=STR          String to print as header
+	    --header-lines=N      The first N lines of the input are treated as header
+	    --header-first        Print header before the prompt line
+	    --ellipsis=STR        Ellipsis to show when line is truncated (default: '..')
 
-  Layout
-    --height=HEIGHT[%]    Display fzf window below the cursor with the given
-                          height instead of using fullscreen
-    --min-height=HEIGHT   Minimum height when --height is given in percent
-                          (default: 10)
-    --layout=LAYOUT       Choose layout: [default|reverse|reverse-list]
-    --border[=STYLE]      Draw border around the finder
-                          [rounded|sharp|horizontal|vertical|
-                           top|bottom|left|right|none] (default: rounded)
-    --margin=MARGIN       Screen margin (TRBL | TB,RL | T,RL,B | T,R,B,L)
-    --padding=PADDING     Padding inside border (TRBL | TB,RL | T,RL,B | T,R,B,L)
-    --info=STYLE          Finder info style [default|inline|hidden]
-    --prompt=STR          Input prompt (default: '> ')
-    --pointer=STR         Pointer to the current line (default: '>')
-    --marker=STR          Multi-select marker (default: '>')
-    --header=STR          String to print as header
-    --header-lines=N      The first N lines of the input are treated as header
-    --header-first        Print header before the prompt line
-    --ellipsis=STR        Ellipsis to show when line is truncated (default: '..')
+	  Display:
+	    --ansi                Enable processing of ANSI color codes
+	    --tabstop=SPACES      Number of spaces for a tab character (default: 8)
+	    --color=COLSPEC       Base scheme (dark|light|16|bw) and/or custom colors
+	    --no-bold             Do NOT use bold text
 
-  Display
-    --ansi                Enable processing of ANSI color codes
-    --tabstop=SPACES      Number of spaces for a tab character (default: 8)
-    --color=COLSPEC       Base scheme (dark|light|16|bw) and/or custom colors
-    --no-bold             Do not use bold text
+	  History
+	    --history=FILE        History file
+	    --history-size=N      Maximum number of history entries (default: 1000)
 
-  History
-    --history=FILE        History file
-    --history-size=N      Maximum number of history entries (default: 1000)
+	  Preview
+	    --preview=COMMAND     Command to preview highlighted line ({})
+	    --preview-window=OPT  Preview window layout (default: right:50%)
+				  [up|down|left|right][,SIZE[%]]
+				  [,[no]wrap][,[no]cycle][,[no]follow][,[no]hidden]
+				  [,border-BORDER_OPT]
+				  [,+SCROLL[OFFSETS][/DENOM]][,~HEADER_LINES]
+				  [,default]
 
-  Preview
-    --preview=COMMAND     Command to preview highlighted line ({})
-    --preview-window=OPT  Preview window layout (default: right:50%)
-                          [up|down|left|right][,SIZE[%]]
-                          [,[no]wrap][,[no]cycle][,[no]follow][,[no]hidden]
-                          [,border-BORDER_OPT]
-                          [,+SCROLL[OFFSETS][/DENOM]][,~HEADER_LINES]
-                          [,default]
+	  Scripting:
+	    -q, --query=STR       Start the finder with the given query
+	    -1, --select-1        Automatically select the only match
+	    -0, --exit-0          Exit immediately when there's no match
+	    -f, --filter=STR      Filter mode. Do NOT start interactive finder.
+	    --print-query         Print query as the first line
+	    --expect=KEYS         Comma-separated list of keys to complete fzf
+	    --read0               Read input delimited by ASCII NUL characters
+	    --print0              Print output delimited by ASCII NUL characters
+	    --sync                Synchronous search for multi-staged filtering
+	    --version             Display version information and exit
 
-  Scripting
-    -q, --query=STR       Start the finder with the given query
-    -1, --select-1        Automatically select the only match
-    -0, --exit-0          Exit immediately when there's no match
-    -f, --filter=STR      Filter mode. Do not start interactive finder.
-    --print-query         Print query as the first line
-    --expect=KEYS         Comma-separated list of keys to complete fzf
-    --read0               Read input delimited by ASCII NUL characters
-    --print0              Print output delimited by ASCII NUL characters
-    --sync                Synchronous search for multi-staged filtering
-    --version             Display version information and exit
-
-  Environment variables
-    FZF_DEFAULT_COMMAND   Default command to use when input is tty
-    FZF_DEFAULT_OPTS      Default options
-                          (e.g. '--layout=reverse --inline-info')
+	  Environment variables:
+	    FZF_DEFAULT_COMMAND   Default command to use when input is tty
+	    FZF_DEFAULT_OPTS      Default options
+				  (e.g. '--layout=reverse --inline-info')
 
 `
 
@@ -348,7 +348,7 @@ func optionalNextString(args []string, i *int) (bool, string) {
 func atoi(str string) int {
 	num, err := strconv.Atoi(str)
 	if err != nil {
-		errorExit("not a valid integer: " + str)
+		errorExit("Not a valid integer: " + str)
 	}
 	return num
 }
@@ -356,7 +356,7 @@ func atoi(str string) int {
 func atof(str string) float64 {
 	num, err := strconv.ParseFloat(str, 64)
 	if err != nil {
-		errorExit("not a valid number: " + str)
+		errorExit("Not a valid number: " + str)
 	}
 	return num
 }
