8eec50d764878628236566e0ef9767688b6de8d0
Christoph Anton Mitterer <mail@christoph.anton.mitterer.name>
2023-09-24 00:04:28 +0200

    [shell] don’t needlessly escape `.` in shell pattern
    `find`’s `-path`-option is described to use shell patterns (i.e. POSIX’ pattern
    matching notation).
    
    In that, `.` is not a special character, thus escaping it shouldn’t be
    necessary.
    
    Signed-off-by: Christoph Anton Mitterer <mail@christoph.anton.mitterer.name>

--- shell/key-bindings.fish
+++ shell/key-bindings.fish
@@ -22,10 +22,10 @@ function fzf_key_bindings
     set -l fzf_query $commandline[2]
     set -l prefix $commandline[3]
 
-    # "-path \$dir'*/\\.*'" matches hidden files/folders inside $dir but not
+    # "-path \$dir'*/.*'" matches hidden files/folders inside $dir but not
     # $dir itself, even if hidden.
     test -n "$FZF_CTRL_T_COMMAND"; or set -l FZF_CTRL_T_COMMAND "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    command find -L \$dir -mindepth 1 \\( -path \$dir'*/.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | sed 's@^\./@@'"
@@ -79,7 +79,7 @@ function fzf_key_bindings
     set -l prefix $commandline[3]
 
     test -n "$FZF_ALT_C_COMMAND"; or set -l FZF_ALT_C_COMMAND "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    command find -L \$dir -mindepth 1 \\( -path \$dir'*/.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
     -o -type d -print 2> /dev/null | sed 's@^\./@@'"
     test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
     begin
