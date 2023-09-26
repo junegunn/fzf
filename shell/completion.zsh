f1d306feab0d6ec91d440c61e54f2280f140de68
Christoph Anton Mitterer <mail@christoph.anton.mitterer.name>
2023-09-26 20:21:09 +0200

    [shell] move username prefixing code where needed
    `__fzf_list_hosts()` seems like a function a user may want to override with some
    custom code.
    For that reason it should be kept as simple as possible, that is printing only
    hostnames, one per line, optionally in some sorting.
    
    The handling of adding a `username@` (which is then the same for each line), if
    any, would unnecessarily complicate that for people who want to override the
    function.
    Therefore this commit moves that to the places where it's actually used (as of
    now only `_fzf_complete_ssh()`).
    
    This also saves any such handling for `_fzf_host_completion()`, where this isn’t
    needed at all.
    
    Right now it comes at a cost, namely an extra invocation of `awk` in the
    `_fzf_complete_ssh()`-case.
    However, it should be easily possible to improve `__fzf_list_hosts()` to no
    longer need the final `awk` in the pipeline there.
    
    Signed-off-by: Christoph Anton Mitterer <mail@christoph.anton.mitterer.name>

--- shell/completion.zsh
+++ shell/completion.zsh
@@ -223,11 +223,11 @@ __fzf_list_hosts() {
   command cat <(command tail -n +1 ~/.ssh/config ~/.ssh/config.d/* /etc/ssh/ssh_config 2> /dev/null | command grep -i '^\s*host\(name\)\? ' | awk '{for (i = 2; i <= NF; i++) print $1 " " $i}' | command grep -v '[*?%]') \
     <(command grep -oE '^[[a-z0-9.,:-]+' ~/.ssh/known_hosts 2> /dev/null | tr ',' '\n' | tr -d '[' | awk '{ print $1 " " $1 }') \
     <(command grep -v '^\s*\(#\|$\)' /etc/hosts 2> /dev/null | command grep -Fv '0.0.0.0') |
-    awk -v "user=$1" '{if (length($2) > 0) {print user $2}}' | sort -u
+    awk '{if (length($2) > 0) {print $2}}' | sort -u
 }
 
 _fzf_complete_telnet() {
-  _fzf_complete +m -- "$@" < <(__fzf_list_hosts "")
+  _fzf_complete +m -- "$@" < <(__fzf_list_hosts)
 }
 
 # The first and the only argument is the LBUFFER without the current word that contains the trigger.
@@ -241,7 +241,7 @@ _fzf_complete_ssh() {
     *)
       local user=
       [[ $prefix =~ @ ]] && user="${prefix%%@*}@"
-      _fzf_complete +m -- "$@" < <(__fzf_list_hosts "$user")
+      _fzf_complete +m -- "$@" < <(__fzf_list_hosts | awk -v user="$user" '{print user $0}')
       ;;
   esac
 }
