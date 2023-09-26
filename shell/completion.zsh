46ee9ac41c53bf6c37637f992691dbadef4ebfbc
Christoph Anton Mitterer <mail@christoph.anton.mitterer.name>
2023-09-26 20:37:04 +0200

    [shell] make `__fzf_list_hosts()` definable by the user
    Just like it’s already done for `_fzf_compgen_path()` and `_fzf_compgen_dir()`
    allow a user to easily define his own version of `__fzf_list_hosts()`.
    
    Also add some documentation on the expected “interface” of such custom function.
    
    Signed-off-by: Christoph Anton Mitterer <mail@christoph.anton.mitterer.name>

--- shell/completion.zsh
+++ shell/completion.zsh
@@ -218,13 +218,18 @@ _fzf_complete() {
   command rm -f "$fifo"
 }
 
-__fzf_list_hosts() {
-  setopt localoptions nonomatch
-  command cat <(command tail -n +1 ~/.ssh/config ~/.ssh/config.d/* /etc/ssh/ssh_config 2> /dev/null | command grep -i '^\s*host\(name\)\? ' | awk '{for (i = 2; i <= NF; i++) print $1 " " $i}' | command grep -v '[*?%]') \
-    <(command grep -oE '^[[a-z0-9.,:-]+' ~/.ssh/known_hosts 2> /dev/null | tr ',' '\n' | tr -d '[' | awk '{ print $1 " " $1 }') \
-    <(command grep -v '^\s*\(#\|$\)' /etc/hosts 2> /dev/null | command grep -Fv '0.0.0.0') |
-    awk '{if (length($2) > 0) {print $2}}' | sort -u
-}
+# To use custom hostname lists, override __fzf_list_hosts.
+# The function is expected to print hostnames, one per line as well as in the
+# desired sorting and with any duplicates removed, to standard output.
+if ! declare -f __fzf_list_hosts > /dev/null; then
+  __fzf_list_hosts() {
+    setopt localoptions nonomatch
+    command cat <(command tail -n +1 ~/.ssh/config ~/.ssh/config.d/* /etc/ssh/ssh_config 2> /dev/null | command grep -i '^\s*host\(name\)\? ' | awk '{for (i = 2; i <= NF; i++) print $1 " " $i}' | command grep -v '[*?%]') \
+      <(command grep -oE '^[[a-z0-9.,:-]+' ~/.ssh/known_hosts 2> /dev/null | tr ',' '\n' | tr -d '[' | awk '{ print $1 " " $1 }') \
+      <(command grep -v '^\s*\(#\|$\)' /etc/hosts 2> /dev/null | command grep -Fv '0.0.0.0') |
+      awk '{if (length($2) > 0) {print $2}}' | sort -u
+  }
+fi
 
 _fzf_complete_telnet() {
   _fzf_complete +m -- "$@" < <(__fzf_list_hosts)
