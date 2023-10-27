f3826fa21f090bb4e50491255f29d91a999a1828
akdevservices <akdevservices@localhost>
2023-10-27 23:05:14 +0300

    Fix #3488

--- shell/completion.bash
+++ shell/completion.bash
@@ -424,7 +424,7 @@ if ! declare -F __fzf_list_hosts > /dev/null; then
     command cat <(command tail -n +1 ~/.ssh/config ~/.ssh/config.d/* /etc/ssh/ssh_config 2> /dev/null | command grep -i '^\s*host\(name\)\? ' | command awk '{for (i = 2; i <= NF; i++) print $1 " " $i}' | command grep -v '[*?%]') \
       <(command grep -oE '^[[a-z0-9.,:-]+' ~/.ssh/known_hosts 2> /dev/null | command tr ',' '\n' | command tr -d '[' | command awk '{ print $1 " " $1 }') \
       <(command grep -v '^\s*\(#\|$\)' /etc/hosts 2> /dev/null | command grep -Fv '0.0.0.0') |
-      command awk '{if (length($2) > 0) {print $2}}' | command sort -u
+      command awk '{for (i = 2; i <= NF; i++) print $i}' | command sort -u
   }
 fi
 
