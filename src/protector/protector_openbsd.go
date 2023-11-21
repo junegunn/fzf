952b6af4454ed55626d78e3845c6b5b640ac831d
Laurent Cheylus <foxy@free.fr>
2023-11-21 07:03:12 +0000

    Allow files creation in /tmp on OpenBSD (#3512)
    - src/protector/protector_openbsd.go: add tmppath for pledge
        permissions
      - fix junegunn/fzf#3511
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- src/protector/protector_openbsd.go
+++ src/protector/protector_openbsd.go
@@ -6,5 +6,5 @@ import "golang.org/x/sys/unix"
 
 // Protect calls OS specific protections like pledge on OpenBSD
 func Protect() {
-	unix.PledgePromises("stdio rpath tty proc exec inet")
+	unix.PledgePromises("stdio rpath tty proc exec inet tmppath")
 }
