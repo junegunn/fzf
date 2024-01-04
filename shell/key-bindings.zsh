36bbaba2a0c0143fe7ed4fe14ddaacfb349d9668
danztran <nguyenbao1917@gmail.com>
2024-01-04 10:35:16 +0700

    fix: number regex

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -103,7 +103,7 @@ fzf-history-widget() {
   local ret=$?
   if [ -n "$selected" ]; then
     num=$selected[1]
-    if [[ "$num" =~ ^-?[0-9]+$ ]]; then
+    if [[ "$num" =~ ^[0-9]+$ ]]; then
       zle vi-fetch-history -n $num
     else # selected is a custom query, not from history
       LBUFFER="${selected[@]}"
