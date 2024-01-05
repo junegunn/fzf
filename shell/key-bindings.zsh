15d30d95eda255df21dca8d94ba7f34d6eeee459
Junegunn Choi <junegunn.c@gmail.com>
2024-01-05 10:44:19 +0900

    Update shell/key-bindings.zsh

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -103,7 +103,7 @@ fzf-history-widget() {
   local ret=$?
   if [ -n "$selected" ]; then
     num=$(awk '{print $1}' <<< "$selected")
-    if [[ "$num" =~ ^[0-9]+$ ]]; then
+    if [[ "$num" =~ ^[1-9][0-9]*$ ]]; then
       zle vi-fetch-history -n $num
     else # selected is a custom query, not from history
       LBUFFER="$selected"
