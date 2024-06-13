e023736c30e789073211ec5b3d49af860c3c3562
Samara Jinnah <tabhooked@gmail.com>
2024-06-13 03:43:33 +0200

    [zsh] Prevent glob expansion in history widget (#3855)

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -107,7 +107,7 @@ fi
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
   local selected num
-  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases noglob 2> /dev/null
   # Ensure the associative history array, which maps event numbers to the full
   # history lines, is loaded, and that Perl is installed for multi-line output.
   if zmodload -F zsh/parameter p:history 2>/dev/null && (( ${#commands[perl]} )); then
