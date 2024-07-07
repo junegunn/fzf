6cbde812f6dc81c35f61fc1f6b88f6a1a6ff1e68
Junegunn Choi <junegunn.c@gmail.com>
2024-07-07 15:14:07 +0900

    [bash] Add code to the default list of programs to support completion
    Close #3843

--- shell/completion.bash
+++ shell/completion.bash
@@ -485,7 +485,7 @@ d_cmds="${FZF_COMPLETION_DIR_COMMANDS-cd pushd rmdir}"
 # NOTE: $FZF_COMPLETION_PATH_COMMANDS and $FZF_COMPLETION_VAR_COMMANDS are
 # undocumented and subject to change in the future.
 a_cmds="${FZF_COMPLETION_PATH_COMMANDS-"
-  awk bat cat diff diff3
+  awk bat cat code diff diff3
   emacs emacsclient ex file ftp g++ gcc gvim head hg hx java
   javac ld less more mvim nvim patch perl python ruby
   sed sftp sort source tail tee uniq vi view vim wc xdg-open
