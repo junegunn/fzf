fb885652cc1f05efee581ed4d2f78f4f48af3fc3
Junegunn Choi <junegunn.c@gmail.com>
2025-01-23 09:43:12 +0900

    Fix RuboCop errors

--- test/test_go.rb
+++ test/test_go.rb
@@ -3010,9 +3010,9 @@ class TestGoFZF < TestBase
 
   def test_info_command_and_focus
     tmux.send_keys(%(seq 100 | #{FZF} --separator x --info-command 'echo $FZF_POS' --bind focus:clear-query), :Enter)
-    tmux.until { |lines| tmux.until { assert_match(%r{^  1 xx}, _1[-2]) } }
+    tmux.until { assert_match(/^  1 xx/, _1[-2]) }
     tmux.send_keys :Up
-    tmux.until { |lines| tmux.until { assert_match(%r{^  2 xx}, _1[-2]) } }
+    tmux.until { assert_match(/^  2 xx/, _1[-2]) }
   end
 
   def test_prev_next_selected
