d67f5006869d50c0e3ba7da5d35587818d3fe1a5
Koichi Murase <myoga.murase@gmail.com>
2025-06-03 21:29:36 +0900

    [bash,zsh] Process hostnames with uppercase letters in known_hosts

--- shell/completion.bash
+++ shell/completion.bash
@@ -503,7 +503,7 @@ if ! declare -F __fzf_list_hosts > /dev/null; then
       ) \
       <(
         __fzf_exec_awk -F ',' '
-          match($0, /^[[a-z0-9.,:-]+/) {
+          match($0, /^[[a-zA-Z0-9.,:-]+/) {
             $0 = substr($0, 1, RLENGTH)
             gsub(/\[/, "")
             for (i = 1; i <= NF; i++)
