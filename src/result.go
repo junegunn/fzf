f86927fd41535ab0b6b6673badbd7b9532d789bf
ipinak <giannis.pin@gmail.com>
2025-08-13 08:46:36 +0300

    use fixed length array when possible

--- src/result.go
+++ src/result.go
@@ -128,7 +128,7 @@ func (result *Result) colorOffsets(matchOffsets []Offset, nthOffsets []Offset, t
 
 	// No ANSI codes
 	if len(itemColors) == 0 && len(nthOffsets) == 0 {
-		var offsets []colorOffset
+		offsets := make([]colorOffset, 0, len(matchOffsets))
 		for _, off := range matchOffsets {
 			offsets = append(offsets, colorOffset{offset: [2]int32{off[0], off[1]}, color: colMatch, match: true})
 		}
