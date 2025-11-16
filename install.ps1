2ab923f3ae04d5e915e5ff4a9cd3bd515bfd1ea5
Junegunn Choi <junegunn.c@gmail.com>
2025-11-16 20:02:39 +0900

    0.67.0

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$version="0.66.1"
+$version="0.67.0"
 
 $fzf_base=Split-Path -Parent $MyInvocation.MyCommand.Definition
 
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/junegunn/fzf/src/protector"
 )
 
-var version = "0.66"
+var version = "0.67"
 var revision = "devel"
 
 //go:embed shell/key-bindings.bash
