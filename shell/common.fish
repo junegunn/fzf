5e475633f2f6dad9dfcdb37fa4e4a2c0a9e3be3e
LM <lalvarezt89@gmail.com>
2025-12-04 15:13:28 +0100

    Restore comments removed by mistake

--- shell/common.fish
+++ shell/common.fish
@@ -1,4 +1,6 @@
   function __fzf_defaults
+    # $argv[1]: Prepend to FZF_DEFAULT_OPTS_FILE and FZF_DEFAULT_OPTS
+    # $argv[2..]: Append to FZF_DEFAULT_OPTS_FILE and FZF_DEFAULT_OPTS
     test -n "$FZF_TMUX_HEIGHT"; or set -l FZF_TMUX_HEIGHT 40%
     string join ' ' -- \
       "--height $FZF_TMUX_HEIGHT --min-height=20+ --bind=ctrl-z:ignore" $argv[1] \
@@ -22,9 +24,12 @@
     set -l prefix ''
     set -l dir '.'
 
+    # Set variables containing the major and minor fish version numbers, using
+    # a method compatible with all supported fish versions.
     set -l -- fish_major (string match -r -- '^\d+' $version)
     set -l -- fish_minor (string match -r -- '^\d+\.(\d+)' $version)[2]
 
+    # fish v3.3.0 and newer: Don't use option prefix if " -- " is preceded.
     set -l -- match_regex '(?<fzf_query>[\s\S]*?(?=\n?$)$)'
     set -l -- prefix_regex '^-[^\s=]+=|^-(?!-)\S'
     if test "$fish_major" -eq 3 -a "$fish_minor" -lt 3
@@ -32,12 +37,16 @@
       set -- match_regex "(?<prefix>$prefix_regex)?$match_regex"
     end
 
+    # Set $prefix and expanded $fzf_query with preserved trailing newlines.
     if test "$fish_major" -ge 4
+      # fish v4.0.0 and newer
       string match -q -r -- $match_regex (commandline --current-token --tokens-expanded | string collect -N)
     else if test "$fish_major" -eq 3 -a "$fish_minor" -ge 2
+      # fish v3.2.0 - v3.7.1 (last v3)
       string match -q -r -- $match_regex (commandline --current-token --tokenize | string collect -N)
       eval set -- fzf_query (string escape -n -- $fzf_query | string replace -r -a '^\\\(?=~)|\\\(?=\$\w)' '')
     else
+      # fish older than v3.2.0 (v3.1b1 - v3.1.2)
       set -l -- cl_token (commandline --current-token --tokenize | string collect -N)
       set -- prefix (string match -r -- $prefix_regex $cl_token)
       set -- fzf_query (string replace -- "$prefix" '' $cl_token | string collect -N)
@@ -45,17 +54,22 @@
     end
 
     if test -n "$fzf_query"
+      # Normalize path in $fzf_query, set $dir to the longest existing directory.
       if test \( "$fish_major" -ge 4 \) -o \( "$fish_major" -eq 3 -a "$fish_minor" -ge 5 \)
+        # fish v3.5.0 and newer
         set -- fzf_query (path normalize -- $fzf_query)
         set -- dir $fzf_query
         while not path is -d $dir
           set -- dir (path dirname $dir)
         end
       else
+        # fish older than v3.5.0 (v3.1b1 - v3.4.1)
         if test "$fish_major" -eq 3 -a "$fish_minor" -ge 2
+          # fish v3.2.0 - v3.4.1
           string match -q -r -- '(?<fzf_query>^[\s\S]*?(?=\n?$)$)' \
             (string replace -r -a -- '(?<=/)/|(?<!^)/+(?!\n)$' '' $fzf_query | string collect -N)
         else
+          # fish v3.1b1 - v3.1.2
           set -- fzf_query (string replace -r -a -- '(?<=/)/|(?<!^)/+(?!\n)$' '' $fzf_query | string collect -N)
           eval set -- fzf_query (string escape -n -- $fzf_query | string replace -r '\\\n$' '')
         end
@@ -66,12 +80,16 @@
       end
 
       if not string match -q -- '.' $dir; or string match -q -r -- '^\./|^\.$' $fzf_query
+        # Strip $dir from $fzf_query - preserve trailing newlines.
         if test "$fish_major" -ge 4
+          # fish v4.0.0 and newer
           string match -q -r -- '^'(string escape --style=regex -- $dir)'/?(?<fzf_query>[\s\S]*)' $fzf_query
         else if test "$fish_major" -eq 3 -a "$fish_minor" -ge 2
+          # fish v3.2.0 - v3.7.1 (last v3)
           string match -q -r -- '^/?(?<fzf_query>[\s\S]*?(?=\n?$)$)' \
             (string replace -- "$dir" '' $fzf_query | string collect -N)
         else
+          # fish older than v3.2.0 (v3.1b1 - v3.1.2)
           set -- fzf_query (string replace -- "$dir" '' $fzf_query | string collect -N)
           eval set -- fzf_query (string escape -n -- $fzf_query | string replace -r -a '^/?|\\\n$' '')
         end
