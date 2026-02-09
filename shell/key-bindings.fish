4e52bb96e9fab762b39f80e2953f7b2488713885
bitraid <bitraid@protonmail.ch>
2026-02-09 17:24:25 +0200

    fish: Add command history preview
    The preview shows the highlighted command and any selected commands,
    after first being formatted and colorized by fish_indent. The timestamp
    of the highlighted command in the preview is converted to strftime
    format "%F %a %T" if the date command is available.
    
    The preview is hidden on start, and is displayed if more than 100
    columns are available and one of the following conditions are met:
    - The highlighted item doesn't completely fit in list view (line
    wrapping is enabled for the preview and is now disabled for the list).
    - The highlighted item contains newlines (multi-line commands or
    strings).
    - The highlighted item contains chained commands in a single line, that
    can be broken down by the formatter for cleaner view.
    - One or more commands are marked as selected.

--- shell/key-bindings.fish
+++ shell/key-bindings.fish
@@ -183,11 +183,32 @@ function fzf_key_bindings
     set -l -- fzf_query (string escape -- $command_line[$current_line])
 
     set -lx -- FZF_DEFAULT_OPTS (__fzf_defaults '' \
-      '--nth=2..,.. --scheme=history --multi --no-multi-line --wrap --wrap-sign="\t\t\t↳ "' \
+      '--nth=2..,.. --scheme=history --multi --no-multi-line --no-wrap --wrap-sign="\t\t\t↳·"' \
       '--bind=\'shift-delete:execute-silent(eval history delete --exact --case-sensitive -- (string escape -n -- {+} | string replace -ra "^\d*\\\\\\t" ""))+reload(eval $FZF_DEFAULT_COMMAND)\'' \
       '--bind="alt-enter:become(string join0 -- (string collect -- {+2..} | fish_indent -i))"' \
       "--bind=ctrl-r:toggle-sort,alt-r:toggle-raw --highlight-line $FZF_CTRL_R_OPTS" \
       '--accept-nth=2.. --delimiter="\t" --tabstop=4 --read0 --print0 --with-shell='(status fish-path)\\ -c)
+
+    # Add dynamic preview options if preview command isn't already set by user
+    if string match -qvr -- '--preview[= ]' "$FZF_DEFAULT_OPTS"
+      # Convert the highlighted timestamp using the date command if available
+      set -l -- date_cmd '{1}'
+      if type -q date
+        if date -d @0 '+%s' 2>/dev/null | string match -q 0
+          # GNU date
+          set -- date_cmd '(date -d @{1} \\"+%F %a %T\\")'
+        else if date -r 0 '+%s' 2>/dev/null | string match -q 0
+          # BSD date
+          set -- date_cmd '(date -r {1} \\"+%F %a %T\\")'
+        end
+      end
+
+      # Prepend the options to allow user customizations
+      set -p -- FZF_DEFAULT_OPTS \
+        '--bind="focus,resize:bg-transform:if test \\"$FZF_COLUMNS\\" -gt 100 -a \\\\( \\"$FZF_SELECT_COUNT\\" -gt 0 -o \\\\( -z \\"$FZF_WRAP\\" -a (string length -- {}) -gt (math $FZF_COLUMNS - 4) \\\\) -o (string collect -- {2..} | fish_indent | count) -gt 1 \\\\); echo show-preview; else echo hide-preview; end"' \
+        '--preview="string collect -- (test \\"$FZF_SELECT_COUNT\\" -gt 0; and string collect -- {+2..}) \\"\\n# \\"'$date_cmd' {2..} | fish_indent --ansi"' \
+        '--preview-window="right,50%,wrap,follow,info,hidden"'
+    end
 
     set -lx FZF_DEFAULT_OPTS_FILE
 
