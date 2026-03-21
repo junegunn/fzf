4348f1e93a1df42f4dae174e042429fb8fb6bd18
bitraid <bitraid@protonmail.ch>
2026-03-21 22:04:52 +0200

    fish: Completion script rewrite (SHIFT-TAB)

--- shell/key-bindings.fish
+++ shell/key-bindings.fish
@@ -12,7 +12,6 @@
 # - $FZF_ALT_C_COMMAND
 # - $FZF_ALT_C_OPTS
 
-
 # Key bindings
 # ------------
 # The oldest supported fish version is 3.1b1. To maintain compatibility, the
@@ -30,12 +29,9 @@ function fzf_key_bindings
     return 1
   end
 
-#----BEGIN INCLUDE common.fish
-# NOTE: Do not directly edit this section, which is copied from "common.fish".
-# To modify it, one can edit "common.fish" and run "./update.sh" to apply
-# the changes. See code comments in "common.fish" for the implementation details.
-
   function __fzf_defaults
+    # $argv[1]: Prepend to FZF_DEFAULT_OPTS_FILE and FZF_DEFAULT_OPTS
+    # $argv[2..]: Append to FZF_DEFAULT_OPTS_FILE and FZF_DEFAULT_OPTS
     test -n "$FZF_TMUX_HEIGHT"; or set -l FZF_TMUX_HEIGHT 40%
     string join ' ' -- \
       "--height $FZF_TMUX_HEIGHT --min-height=20+ --bind=ctrl-z:ignore" $argv[1] \
@@ -54,51 +50,17 @@ function fzf_key_bindings
     end
   end
 
-  function __fzf_cmd_tokens -d 'Return command line tokens, skipping leading env assignments and command prefixes'
-    set -l tokens
-    if test (string match -r -- '^\d+' $version) -ge 4
-      set -- tokens (commandline -xpc)
-    else
-      set -- tokens (commandline -opc)
-    end
-
-    set -l -- var_count 0
-    for i in $tokens
-      if string match -qr -- '^[\w]+=' $i
-        set var_count (math $var_count + 1)
-      else
-        break
-      end
-    end
-    set -e -- tokens[0..$var_count]
-
-    while true
-      switch "$tokens[1]"
-        case builtin command
-          set -e -- tokens[1]
-          test "$tokens[1]" = "--"; and set -e -- tokens[1]
-        case env
-          set -e -- tokens[1]
-          test "$tokens[1]" = "--"; and set -e -- tokens[1]
-          while string match -qr -- '^[\w]+=' "$tokens[1]"
-            set -e -- tokens[1]
-          end
-        case '*'
-          break
-      end
-    end
-
-    string escape -n -- $tokens
-  end
-
   function __fzf_parse_commandline -d 'Parse the current command line token and return split of existing filepath, fzf query, and optional -option= prefix'
     set -l fzf_query ''
     set -l prefix ''
     set -l dir '.'
 
+    # Set variables containing the major and minor fish version numbers, using
+    # a method compatible with all supported fish versions.
     set -l -- fish_major (string match -r -- '^\d+' $version)
     set -l -- fish_minor (string match -r -- '^\d+\.(\d+)' $version)[2]
 
+    # fish v3.3.0 and newer: Don't use option prefix if " -- " is preceded.
     set -l -- match_regex '(?<fzf_query>[\s\S]*?(?=\n?$)$)'
     set -l -- prefix_regex '^-[^\s=]+=|^-(?!-)\S'
     if test "$fish_major" -eq 3 -a "$fish_minor" -lt 3
@@ -106,12 +68,16 @@ function fzf_key_bindings
       set -- match_regex "(?<prefix>$prefix_regex)?$match_regex"
     end
 
+    # Set $prefix and expanded $fzf_query with preserved trailing newlines.
     if test "$fish_major" -ge 4
+      # fish v4.0.0 and newer
       string match -q -r -- $match_regex (commandline --current-token --tokens-expanded | string collect -N)
     else if test "$fish_major" -eq 3 -a "$fish_minor" -ge 2
+      # fish v3.2.0 - v3.7.1 (last v3)
       string match -q -r -- $match_regex (commandline --current-token --tokenize | string collect -N)
       eval set -- fzf_query (string escape -n -- $fzf_query | string replace -r -a '^\\\(?=~)|\\\(?=\$\w)' '')
     else
+      # fish older than v3.2.0 (v3.1b1 - v3.1.2)
       set -l -- cl_token (commandline --current-token --tokenize | string collect -N)
       set -- prefix (string match -r -- $prefix_regex $cl_token)
       set -- fzf_query (string replace -- "$prefix" '' $cl_token | string collect -N)
@@ -119,17 +85,22 @@ function fzf_key_bindings
     end
 
     if test -n "$fzf_query"
+      # Normalize path in $fzf_query, set $dir to the longest existing directory.
       if test \( "$fish_major" -ge 4 \) -o \( "$fish_major" -eq 3 -a "$fish_minor" -ge 5 \)
+        # fish v3.5.0 and newer
         set -- fzf_query (path normalize -- $fzf_query)
         set -- dir $fzf_query
         while not path is -d $dir
           set -- dir (path dirname $dir)
         end
       else
+        # fish older than v3.5.0 (v3.1b1 - v3.4.1)
         if test "$fish_major" -eq 3 -a "$fish_minor" -ge 2
+          # fish v3.2.0 - v3.4.1
           string match -q -r -- '(?<fzf_query>^[\s\S]*?(?=\n?$)$)' \
             (string replace -r -a -- '(?<=/)/|(?<!^)/+(?!\n)$' '' $fzf_query | string collect -N)
         else
+          # fish v3.1b1 - v3.1.2
           set -- fzf_query (string replace -r -a -- '(?<=/)/|(?<!^)/+(?!\n)$' '' $fzf_query | string collect -N)
           eval set -- fzf_query (string escape -n -- $fzf_query | string replace -r '\\\n$' '')
         end
@@ -140,12 +111,16 @@ function fzf_key_bindings
       end
 
       if not string match -q -- '.' $dir; or string match -q -r -- '^\./|^\.$' $fzf_query
+        # Strip $dir from $fzf_query - preserve trailing newlines.
         if test "$fish_major" -ge 4
+          # fish v4.0.0 and newer
           string match -q -r -- '^'(string escape --style=regex -- $dir)'/?(?<fzf_query>[\s\S]*)' $fzf_query
         else if test "$fish_major" -eq 3 -a "$fish_minor" -ge 2
+          # fish v3.2.0 - v3.7.1 (last v3)
           string match -q -r -- '^/?(?<fzf_query>[\s\S]*?(?=\n?$)$)' \
             (string replace -- "$dir" '' $fzf_query | string collect -N)
         else
+          # fish older than v3.2.0 (v3.1b1 - v3.1.2)
           set -- fzf_query (string replace -- "$dir" '' $fzf_query | string collect -N)
           eval set -- fzf_query (string escape -n -- $fzf_query | string replace -r -a '^/?|\\\n$' '')
         end
@@ -154,7 +129,6 @@ function fzf_key_bindings
 
     string escape -n -- "$dir" "$fzf_query" "$prefix"
   end
-#----END INCLUDE
 
   # Store current token in $dir as root for the 'find' command
   function fzf-file-widget -d "List files and folders"
@@ -171,7 +145,7 @@ function fzf_key_bindings
     set -lx FZF_DEFAULT_OPTS_FILE
 
     set -l result (eval (__fzfcmd) --walker-root=$dir --query=$fzf_query | string split0)
-    and commandline -rt -- (string join -- ' ' $prefix(string escape --no-quoted -- $result))' '
+    and commandline -rt -- (string join -- ' ' $prefix(string escape -n -- $result))' '
 
     commandline -f repaint
   end
